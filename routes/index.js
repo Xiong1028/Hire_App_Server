1be621b0a438d8f0db3e8586abafa5829bc68eb3
xiong1028 <lixiong1124@gmail.com>
2019-01-11 18:33:43 -0800

    add nodemon in devDependencies

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+//define a route for registering new users
+/*
+*   path:/register
+*   method: POST
+*   arguments: username, password
+*   return onSuccess:{code:0,data:{_id:'abc',username:'xxx',password:'123'}
+*   return onFail:{code:1,msg:'username is existed'}
+*   Note: admin is existed
+*
+* */
+router.post('/register', (req, res) => {
+  //get the request object
+  const {username, password} = req.body;
+  if (username === 'admin') {
+    //return onFail
+    res.send({code: 1, msg: 'username is existed'});
+  } else {
+    //return onSuccess
+    res.send({code: 0, data:{id: 'abc', username, password}});
+  }
+})
+
 module.exports = router;
